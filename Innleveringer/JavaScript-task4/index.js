5a974263892fc79ffa91565c6e48634dd758198c
Christian R <christianriis.kodehode@gmail.com>
2022-11-23 19:31:01 +0100

    styling_mediaq

--- Innleveringer/JavaScript-task4/index.js
+++ Innleveringer/JavaScript-task4/index.js
@@ -7,6 +7,9 @@ const enterKey = (event) => {
 };
 */
 
+// GJØRE OM ET TALL TIL TEKST
+// (F.eks. "257" blir til "TO HUNDRE OG FEMTISYV")
+
 const numToText = (input = document.getElementById("num-in").value) => {
   const inputArray = input.split("");
   let out = "";
@@ -83,5 +86,97 @@ const numToText = (input = document.getElementById("num-in").value) => {
     out = err;
   }
 
+  if (input[0] == "0") {
+    out = err;
+  }
+
   document.getElementById("ans").innerText = `${out}`;
 };
+
+// LAGE SIRKLER MED TALL INNI (DIV'ER) FOR ALLE PRIMTALL MELLOM 2 OG ET GITT INPUT-TALL
+
+// const drawPrimes = (input = document.getElementById("num-two").value) => {
+//   //
+//   let primeArray = [2, 3];
+//   let isPrime = true;
+
+//   if (Number(input) < 2) {
+//     console.log("Input må være minimum 2.");
+//   } else if (Number(input) > 999) {
+//     console.log("Input kan ikke være større enn 999.");
+//   } else {
+//     console.log(`Input er "${input}"`);
+//     for (let i = 2; i < Number(input); i++) {}
+//     console.log(`primeArray: ${primeArray}`);
+//   }
+
+// if (Number(input) < 2) {
+//   console.log("Input må være minimum 2.");
+// } else if (Number(input) > 999) {
+//   console.log("Input kan ikke være større enn 999.");
+// } else {
+//   for (let i = 0; i < Number(input); i++) {
+//     record.push(1);
+//   }
+//   console.log(`record: ${record}`);
+//   for (let prime = 2; prime < max + 1; prime++) {
+//     if (record[prime]) {
+//       for (let m = prime * prime; m < Number(input); m += prime) {
+//         record[m] = 0;
+//       }
+//     }
+//   }
+//   console.log(`record: ${record}`);
+//   for (let checked = 2; checked < Number(input); checked++) {
+//     if (record[checked]) {
+//       primes.push[checked];
+//     }
+//   }
+//   console.log(`record: ${record}`);
+//   console.log(`primes: ${primes}`);
+//   return primes;
+// }
+// };
+// MAKE CIRCLES
+
+// HTML
+
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Document</title>
+//     <link rel="stylesheet" href="style.css" />
+//     <script src="main.js" defer></script>
+//   </head>
+//   <body>
+//     <input id="inputNum" type="number" />
+
+//     <button onclick="makeCircles()">Lag sirkler</button>
+//     <div id="circleContainer"></div>
+//   </body>
+// </html>
+
+// JAVASCRIPT
+
+// const inputNum = document.querySelector("#inputNum");
+// const circleContainer = document.querySelector("#circleContainer");
+// const makeCircles = () => {
+//   while (circleContainer.firstChild) {
+//     circleContainer.removeChild(circleContainer.firstChild);
+//   }
+//   if (inputNum.value && inputNum.value < 1000) {
+//     for (let i = 0; i < inputNum.value; i++) {
+//       let circle = document.createElement("div");
+//       circle.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+//       circle.classList.add("circle");
+//       circle.textContent = i + 1;
+//       circleContainer.appendChild(circle);
+//     }
+//   } else {
+//     window.alert("Please input a number between 1 and 1000");
+//   }
+// };
+//
